8978a2aa364338c8493a74af20f52d9cdf2b1822
yshir <yshir0020@gmail.com>
2022-01-30 23:11:48 +0900

    Impl server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+
+use axum::{response::IntoResponse, routing, Json, Router};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    init_logger();
+
+    let app = Router::new().route("/", routing::get(ping));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    tracing::info!("🚀 Server running on http://{}", addr);
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+
+    Ok(())
+}
+
+async fn ping() -> impl IntoResponse {
+    Json(serde_json::json!({ "ok": true }))
+}
+
+fn init_logger() {
+    if std::env::var_os("RUST_LOG").is_none() {
+        std::env::set_var("RUST_LOG", "rust_heroku_deploy=debug");
+    }
+    tracing_subscriber::fmt::init();
 }
